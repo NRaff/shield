6eda26ac7f45e5b6d649a749a6e52e47288b1a3f
nraff <nraff510@gmail.com>
2021-10-30 17:38:50 -0700

    movement without clearing

--- src/scripts/tank.js
+++ src/scripts/tank.js
@@ -8,7 +8,6 @@ class Tank extends PhysObject {
     super(ctx, options);
     this.color = 'darkgreen';
     this.shield = new Shield(this.context, this.pos,'cyan',ArcType.eighth,this.size);
-    this.raf;
     this.speed = {x: 15, y: 15};
     this.controls = this.setControls();
 
@@ -20,18 +19,23 @@ class Tank extends PhysObject {
     this.canvas.addEventListener('keydown', (keyEvent) => {
       let keyPressed = keyEvent.key
       if (Object.keys(this.controls).includes(keyPressed)) { 
+        this.clearAround()
         this.controls[keyPressed]()
-        this.redraw();
       }
-      raf = window.requestAnimationFrame(this.redraw.bind(this));
+      window.requestAnimationFrame(this.redraw.bind(this));
     })
   }
 
   redraw() {
-    this.context.clearRect(0,0,this.canvas.width, this.canvas.height)
     this.shield.updatePos();
     this.draw();
     this.shield.draw()
+  }
+  clearAround() {
+    this.context.clearRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+    this.context.clearRect(this.pos.x, this.pos.y, -this.size.x, this.size.y);
+    this.context.clearRect(this.pos.x, this.pos.y, this.size.x, -this.size.y);
+    this.context.clearRect(this.pos.x, this.pos.y, -this.size.x, -this.size.y);
   }
 
   setControls() {
